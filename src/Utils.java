42a83ff49e8b18d56ea802e6ac2cd9517c504f0c
Tsivka Galina <galinatsivka@gmail.come>
2020-12-22 22:06:11 +0200

    first commit

---
+++ src/Utils.java
@@ -0,0 +1,8 @@
+public class Utils {
+    private static final String URL = "http://localhost:8080/HWPro4ChatServer_war_exploded";
+    private static final int PORT = 8080;
+
+    public static String getURL() {
+        return URL ;
+    }
+}
